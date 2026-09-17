10cf9a0b8b32ca26132142961c96d18e38c30b86
João Victor <victorlopesdemedeirosjoao@gmail.com>
2026-09-17 12:12:14 -0300

    Projeto para pratica de ralacionamento JPA

---
+++ src/main/java/Api/company/relationships/RelationshipsApplication.java
@@ -0,0 +1,13 @@
+package Api.company.relationships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationshipsApplication.class, args);
+	}
+
+}
