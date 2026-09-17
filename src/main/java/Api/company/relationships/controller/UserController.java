029ab71cb5f9bb77615d2601e6943f784e9b522c
João Victor <victorlopesdemedeirosjoao@gmail.com>
2026-09-17 15:16:22 -0300

    criacao completa do metodo crud da api

--- src/main/java/Api/company/relationships/controller/UserController.java
+++ src/main/java/Api/company/relationships/controller/UserController.java
@@ -7,10 +7,7 @@ import Api.company.relationships.servise.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,5 +31,23 @@ public class UserController {
     public ResponseEntity<List<User>> getAllUsers(){
         var user = userService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable Long id){
+        var user = userService.findById(id);
+        return  ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteById(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).body("Sucessfully deleted");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody UserDto dto){
+        var userUpdate = userService.update(id, dto);
+        return ResponseEntity.status(HttpStatus.OK).body(userUpdate);
     }
 }
