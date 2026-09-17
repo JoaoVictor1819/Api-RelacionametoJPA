4bd15328617b303485cf3631c7c69022ddacaa72
João Victor <victorlopesdemedeirosjoao@gmail.com>
2026-09-17 19:20:54 -0300

    Feita os complemetos para uma organizacao na documetacao no swagger

--- src/main/java/Api/company/relationships/controller/UserController.java
+++ src/main/java/Api/company/relationships/controller/UserController.java
@@ -4,6 +4,8 @@ package Api.company.relationships.controller;
 import Api.company.relationships.database.entity.User;
 import Api.company.relationships.dto.UserDto;
 import Api.company.relationships.servise.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -25,30 +27,35 @@ public class UserController {
     }
 
     @PostMapping
+    @Operation(summary = "Cadastro de Usuarios", description = "EndPoint feito para Cadastrar Usuario e Salvar")
     public ResponseEntity<User> createUser(@RequestBody @Valid UserDto dto){
         var user = userService.save(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
     @GetMapping
+    @Operation(summary = "Buscar todos", description = "EndPoint que chama todos os usuarios dentro do banco")
     public ResponseEntity<List<User>> getAllUsers(){
         var user = userService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Buscar por ID", description = "EndPoint que buscar o usuario pelo id")
     public ResponseEntity<User> getById(@PathVariable Long id){
         var user = userService.findById(id);
         return  ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deleta um usario", description = "EndPoint que delete um usuario por id")
     public ResponseEntity deleteById(@PathVariable Long id){
         userService.delete(id);
         return ResponseEntity.status(HttpStatus.OK).body("Sucessfully deleted");
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualizar Usuario", description = "EndPoint que atualizar o usuario com o id especifico")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody @Valid UserDto dto){
         var userUpdate = userService.update(id, dto);
         return ResponseEntity.status(HttpStatus.OK).body(userUpdate);
